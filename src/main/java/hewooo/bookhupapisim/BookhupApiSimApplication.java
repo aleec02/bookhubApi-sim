c070c916edf30c8089dfc262f3e49ff13e871ab8
aleec02 <conzahualpa@gmail.com>
2024-09-20 14:08:33 -0500

    config: estructura inicial del proyecto

---
+++ src/main/java/hewooo/bookhupapisim/BookhupApiSimApplication.java
@@ -0,0 +1,13 @@
+package hewooo.bookhupapisim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookhupApiSimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookhupApiSimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hewooo/bookhupapisim/BookhupApiSimApplicationTests.java
@@ -0,0 +1,13 @@
+package hewooo.bookhupapisim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookhupApiSimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
