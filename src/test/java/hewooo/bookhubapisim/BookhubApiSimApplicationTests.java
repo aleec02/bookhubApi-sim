db1a69b58dfbc024aaec798d02e95724ed78a5fc
aleec02 <conzahualpa@gmail.com>
2024-09-29 12:05:36 -0500

    refactor(app-name): change name to `bookhub`

--- src/test/java/hewooo/bookhubapisim/BookhubApiSimApplicationTests.java
+++ src/test/java/hewooo/bookhubapisim/BookhubApiSimApplicationTests.java
@@ -1,10 +1,10 @@
-package hewooo.bookhupapisim;
+package hewooo.bookhubapisim;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BookhupApiSimApplicationTests {
+class BookhubApiSimApplicationTests {
 
     @Test
     void contextLoads() {
